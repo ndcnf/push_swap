b20aecf17fb6a70e942ae44cfa23174fd642c02e
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2022-04-28 10:30:58 +0200

    one_string() OK

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 18:09:04 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/04/26 15:44:22 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/04/28 09:52:46 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ void	init_stacks(t_data *d)
 	d->b->i = 0;
 	d->a->nbr = 0;
 	d->b->nbr = 0;
-	d->a->v = NULL;
-	d->b->v = NULL;
+	//d->a->v = {{0}};
+	//d->b->v = {{0}};
 	d->arg = 0;
 }
 
@@ -38,21 +38,21 @@ void	pop_stack_a(t_data *d, char *argv[])
 	if (argv[2] == NULL)
 	{
 		d->arg = 1;
-		ft_printf("Une seule string ? arg = [%d]\n", d->arg);
+		ft_printf("Une seule string ? type arg : %d\n", d->arg);
 		one_string(d, argv);
 	}
-	ft_printf("Alors, une seule ? arg = [%d]\n", d->arg);
+	ft_printf("Alors, une seule ? type arg : %d\n", d->arg);
 	if (d->arg == 0)
 	{
 		//every_arg_counts();
 
-		ft_printf("arg = [%d]\n", d->arg);
+/* 		ft_printf("arg = [%d]\n", d->arg);
 		while (argv[d->a->i])
 		{
 			ft_printf("et la, l'arg est de... [%d]\n", d->arg);
-			d->a->v[d->a->i] = (long)(argv[d->a->i]);
+			d->a->v[d->a->i] = (long)(argv[d->a->i])[i];
 			d->a->nbr++;
 			d->a->i++;
-		}
+		} */
 	}
 }
--- inputs.c
+++ inputs.c
@@ -6,7 +6,7 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 12:39:55 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/04/27 15:42:14 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/04/28 10:30:33 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,26 @@
 void	one_string(t_data *d, char *argv[])
 {
 	int	i;
+	int	lenny;
 
 	i = 0;
 	d->a->i = 0;
-
-	i = 0;
-	//Trouver la bonne condition pour la boucle ici
-	while (argv[1][i] != 0)
-	{
-		d->a->v[d->a->i] = (long *)ft_split(argv[1], ' ')[i];
-		ft_printf("valeur [%s] - i struct [%d] - i [%d]\n", d->a->v[d->a->i], d->a->i, i);
-		i++;
-		d->a->i++;
-		d->a->nbr++;
-	}
-	ft_printf("%d\n", d->a->nbr);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	//i = word_cnt(argv[1], ' ');
-	//ft_printf("%d", i);
-
-/* 	while (argv[1][i])
+	lenny = ft_strlen(argv[1]);
+	while ((size_t)i <= ft_strlen(argv[1]))
 	{
 		if (argv[1][i] == ' ')
-		{
 			d->a->nbr++;
-			d->a->i++;
-
-		}
-		ft_printf()
+		i++;
 	}
-	while (argv[1])
+	d->a->nbr++;
+	lenny = d->a->nbr;
+	i = 0;
+	while (d->a->i < lenny)
 	{
-		ft_printf("[%s]\n", argv[1]);
-		ft_printf("[%d]\n", ft_split(argv[1], ' '));
-		d->a->nbr++;
+		d->a->v[d->a->i] = (long *)ft_split(argv[1], ' ')[i];
+		i++;
 		d->a->i++;
-		//segfault ici....:
-		//d->a->v[d->a->i] = (int)ft_split(argv[i++], ' ');
 	}
-	d->arg = 1; */
 }
 
 void	every_arg_counts(t_data *d, int argc, char *argv[])
